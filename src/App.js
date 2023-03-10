7ac535af2def1d4437d525dca7804446efa51560
Xuan Phao <nguyenthanhhoa075@gmail.com>
2023-03-11 01:14:33 +0700

    compilation-targets

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <img
           src={logo}
-          className='App-logo'
-          alt='logo'
+          className="App-logo"
+          alt="logo"
         />
-        <h1>Hello anh em!</h1>
+        <h1>Hello anh em F8!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Em là Xuân Pháo.
         </p>
         <a
-          className='App-link'
-          href='https://www.youtube.com/channel/UCxvQ4j_oWcUrUkGbHWs4dLw'
-          target='_blank'
-          rel='noopener noreferrer'>
+          className="App-link"
+          href="https://www.youtube.com/channel/UCxvQ4j_oWcUrUkGbHWs4dLw"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Hello youtube!
         </a>
       </header>
